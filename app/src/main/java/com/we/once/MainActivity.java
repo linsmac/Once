ffbea90f9ca05fb5dc32356927f6d56ba66efdc4
linyongsyuan <herewego8520@outlook.com>
2019-10-12 19:09:54 +0800

    Qrcode scanner

--- app/src/main/java/com/we/once/MainActivity.java
+++ app/src/main/java/com/we/once/MainActivity.java
@@ -1,4 +1,5 @@
 package com.we.once;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +9,14 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.uuzuche.lib_zxing.activity.CaptureActivity;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
 import com.we.once.Spinner_main;
 
 import java.text.SimpleDateFormat;
@@ -22,28 +27,33 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinner;
     private Spinner_main spinner_main;
     private TextView date;
+
     boolean logon = false;
     TextView dateTextView;
 
+    private final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+    }
 
-
-
-
-        ImageView addPageBtn = (ImageView)findViewById(R.id.imageView3);
+    public void findView() {
+        ImageView addPageBtn = (ImageView) findViewById(R.id.imageView3);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
         addPageBtn.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
+
             public void onClick(View view) {
 
-
+                TextView dateString = findViewById(R.id.dateTextView);
                 Intent intent3 = new Intent();
-                intent3.setClass(MainActivity.this,AddView.class);
+                intent3.setClass(MainActivity.this, AddView.class);
+                intent3.putExtra("date", dateString.getText().toString());
                 startActivity(intent3);
             }
 
@@ -51,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        ImageView inquirePageBtn = (ImageView)findViewById(R.id.inquire);
+        ImageView inquirePageBtn = (ImageView) findViewById(R.id.inquire);
         inquirePageBtn.setOnClickListener(new View.OnClickListener() {
 
 
@@ -60,13 +70,12 @@ public class MainActivity extends AppCompatActivity {
 
 
                 Intent intent1 = new Intent();
-                intent1.setClass(MainActivity.this,InquireActivity.class);
+                intent1.setClass(MainActivity.this, InquireActivity.class);
                 startActivity(intent1);
             }
 
 
         });
-
 
 
         TextView moneyback = findViewById(R.id.moneyback);//接收AddView的money數值
@@ -103,19 +112,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
+    public void openQRcode(View view) {
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
     private String getDateFormat(long time) {
         Date curDate = new Date(time);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日");
         return formatter.format(curDate);
     }
 
-
-
-
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (null != data) {
+                Bundle bundle = data.getExtras();
+                if (bundle == null) {
+                    return;
+                }
+                if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
+                    String result = bundle.getString(CodeUtils.RESULT_STRING);
+                    Intent intent = new Intent();
+                    intent.setAction("android.intent.action.VIEW");
+//                    Uri content_url = Uri.parse(result.toString());
+//                    intent.setData(content_url);
+                    Toast.makeText(MainActivity.this,
+                            result, Toast.LENGTH_LONG).show();
+                    startActivity(intent);
+                } else if (bundle.getInt(CodeUtils.RESULT_TYPE)
+                        == CodeUtils.RESULT_FAILED) {
+                    Toast.makeText(MainActivity.this,
+                            "解析二維碼失敗", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 
 
 }
